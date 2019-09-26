a6b05d920c0d256c24ac08b2c737060c01870398
daazarov <daazarov@telecom.by>
2019-09-26 23:59:49 +0300

    edit api links

--- src/Web/WebMvc/SDmS.Mvc.Infrastructure/API.cs
+++ src/Web/WebMvc/SDmS.Mvc.Infrastructure/API.cs
@@ -11,17 +11,17 @@
 
             public static string ConfirmEmail(string baseUrl, string userId, string code)
             {
-                return string.Format("{0}api/accounts/confirm_email?userId={1}&code={2}", baseUrl, userId, code);
+                return string.Format("{0}/api/accounts/confirm_email?userId={1}&code={2}", baseUrl, userId, code);
             }
 
             public static string ForgotPassword(string baseUrl)
             {
-                return string.Format("{0}api/accounts/forgot_password", baseUrl);
+                return string.Format("{0}/api/accounts/forgot_password", baseUrl);
             }
 
             public static string ResetPassword(string baseUrl)
             {
-                return string.Format("{0}api/accounts/reset_password", baseUrl);
+                return string.Format("{0}/api/accounts/reset_password", baseUrl);
             }
         }
     }

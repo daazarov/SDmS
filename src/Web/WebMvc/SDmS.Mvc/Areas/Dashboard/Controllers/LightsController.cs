c264d654d677de59a5209a745f2bd09a845f5b7f
daazarov <daazarov@telecom.by>
2019-10-02 00:28:39 +0300

    commit

--- src/Web/WebMvc/SDmS.Mvc/Areas/Dashboard/Controllers/LightsController.cs
+++ src/Web/WebMvc/SDmS.Mvc/Areas/Dashboard/Controllers/LightsController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SDmS.Mvc.Attributes.Filters;
 
 namespace SDmS.Mvc.Areas.Dashboard.Controllers
 {
-    public class LightsController : BaseDashboardController
+    [DashboardAuthorization]
+	public class LightsController : BaseDashboardController
     {
         // GET: Dashboard/Lights
         public ActionResult Index()
